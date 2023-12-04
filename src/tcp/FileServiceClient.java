51c0ee41f7b273cd859e8210e4b2786edce3c3a5
corey <bcorey395@gmail.com>
2023-12-04 12:44:30 -0500

    Implement Upload command with multithreading

--- src/tcp/FileServiceClient.java
+++ src/tcp/FileServiceClient.java
@@ -2,94 +2,60 @@ package tcp;
 
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
-import java.nio.file.Files;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class FileServiceClient {
+    private static String fileName;
+    private static ByteBuffer request;
+    private static int bytesToRead = 0;
+    private static ByteBuffer statusCode;
+    private static byte[] a;
+    private static Scanner keyboard = new Scanner(System.in);
+    private static String command;
+
+    private static SocketChannel channel;
+
+    private static ExecutorService executor = Executors.newFixedThreadPool(2);
+
     public static void main(String[] args) throws Exception {
         if(args.length != 2) {
             System.out.println("Syntax: FileClient <ServerIP> <ServerPort>");
             return;
         }
+
         //Convert port into an Integer
         int serverPort = Integer.parseInt(args[1]);
-        String command;
+//        String command;
 
         do {
-            Scanner keyboard = new Scanner(System.in);
+//            Scanner keyboard = new Scanner(System.in);
             System.out.println("\nPlease type a command: ");
             command = keyboard.nextLine();
-
             //Sending request data to server
             ByteBuffer buffer = ByteBuffer.wrap(command.getBytes());
-            SocketChannel channel = SocketChannel.open();
+            channel = SocketChannel.open();
             channel.connect(new InetSocketAddress(args[0], serverPort));
             channel.write(buffer);
 
 
             //CREATE A NEW channel for every command. Close half the connection after we get the data from the user.
 
-            String fileName;
-            ByteBuffer request;
-            int bytesToRead = 0;
-            ByteBuffer statusCode;
-            byte[] a;
             switch(command) {
                 case "G":
-                    System.out.println("Enter a file name to download");
-                    fileName = keyboard.nextLine();
-                    request = ByteBuffer.wrap((fileName.getBytes()));
-                    channel.write(request);
-                    //Implement file download functionality
-
-                    channel.shutdownOutput();
-                    bytesToRead = 17;
-                    statusCode = ByteBuffer.allocate(bytesToRead);
-
-                    while((bytesToRead -= channel.read(statusCode)) > 0);
-                    statusCode.flip();
-                    a = new byte[17];
-                    statusCode.get(a);
-                    System.out.println(new String(a));
+                    executor.submit(() -> {
+                        try{
+                            downloadFile();
+                        }catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
                     break;
                 case "U":
-                    System.out.println("Enter a file name to be uploaded");
-                    fileName = keyboard.nextLine();
-                    File fileToUpload = new File(fileName);
-                    if (!fileToUpload.exists() || !fileToUpload.isFile()) {
-                        System.out.println("Invalid file path.");
-                        break;
-                    }
-
-                    // Send the filename first with a separator
-                    ByteBuffer fileNameBuffer = ByteBuffer.wrap((fileName + "%").getBytes());
-                    channel.write(fileNameBuffer);
-
-                    // Read and send file in chunks
-                    byte[] bufferArray = new byte[1000];
-                    FileInputStream fis = new FileInputStream(fileToUpload);
-                    int bytesRead;
-                    while ((bytesRead = fis.read(bufferArray)) != -1) {
-                        System.out.println(bytesRead);
-                        ByteBuffer fileChunkBuffer = ByteBuffer.wrap(bufferArray, 0, bytesRead);
-                        channel.write(fileChunkBuffer);
-                    }
-                    fis.close();
-                    channel.shutdownOutput();
-                    bytesToRead = 1;
-                    statusCode = ByteBuffer.allocate(bytesToRead);
-
-                    while((bytesToRead -= channel.read(statusCode)) > 0);
-                    statusCode.flip();
-                    a = new byte[1];
-                    statusCode.get(a);
-                    System.out.println(new String(a));
-                    //Implement functionality for file upload procedure
+                    uploadProcess();
                     break;
-
                 case "D":
                     System.out.println("Enter a file name to be deleted: ");
                     fileName = keyboard.nextLine();
@@ -118,6 +84,7 @@ public class FileServiceClient {
                     a = new byte[1];
                     statusCode.get(a);
                     System.out.println(new String(a));
+
                     break;
                 case "R":
                     System.out.println("Enter the old file name to rename: ");
@@ -142,9 +109,9 @@ public class FileServiceClient {
                     System.out.println(new String(a));
                     break;
                 default:
-                        if(!command.equals("0")) {
-                            System.out.println("Unknown command");
-                        }
+                    if(!command.equals("0")) {
+                        System.out.println("Unknown command");
+                    }
 
             }
 
@@ -154,8 +121,8 @@ public class FileServiceClient {
             if (bytesRead > 0) {
                 responseBuffer.flip();
 //                char firstByte = (char) responseBuffer.get();
-                byte[] responseData = new byte[responseBuffer.remaining()];
-                responseBuffer.get(responseData);
+                byte[] responseData = new byte[bytesRead];
+                responseBuffer.get(responseData, 0, responseData.length);
                 String response = new String(responseData);
                 switch (command) {
                     case "G":
@@ -178,7 +145,93 @@ public class FileServiceClient {
                 }
             }
         }while(!command.equals("0"));
+    }
+
+    public static void downloadFile() throws IOException {
+        System.out.println("Enter a file name to download");
+        fileName = keyboard.nextLine();
+        request = ByteBuffer.wrap((fileName.getBytes()));
+
+        channel.write(request);
+        //Implement file download functionality
 
 
+        channel.shutdownOutput();
+        bytesToRead = 17;
+        statusCode = ByteBuffer.allocate(bytesToRead);
+//        channel.read(statusCode);
+
+        while((bytesToRead -= channel.read(statusCode)) > 0);
+        statusCode.flip();
+        a = new byte[17];
+        statusCode.get(a);
+        System.out.println(new String(a));
+
+//        if(statusString.equals("S")) {
+//            ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+//            channel.read(lengthBuffer);
+//            lengthBuffer.flip();
+//            int contentLength = lengthBuffer.getInt();
+//
+//            ByteBuffer fileContentBuffer = ByteBuffer.allocate(contentLength);
+//            channel.read(fileContentBuffer);
+//            byte[] fileContent = fileContentBuffer.array();
+//
+//            File downloadedFile = new File("DownloadedFiles/" + fileName);
+//            try (FileOutputStream fos = new FileOutputStream(downloadedFile)) {
+//                fos.write(fileContent);
+//            }
+//
+//            System.out.printf("File downloaded successfully: %s%n", downloadedFile.getAbsolutePath());
+//        }else {
+//            System.out.printf("Failed to download file: %s%n", fileName);
+//
+//        }
+    }
+
+    public static void uploadProcess() {
+        System.out.println("Enter a file name to be uploaded");
+        fileName = keyboard.nextLine();
+        File fileToUpload = new File(fileName);
+        if (!fileToUpload.exists() || !fileToUpload.isFile()) {
+            System.out.println("Invalid file path.");
+            return;
+        }
+
+        executor.submit(() -> {
+            try {
+                uploadFile(fileToUpload);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void uploadFile(File fileToUpload) throws IOException {
+        // Send the filename first with a separator
+        ByteBuffer fileNameBuffer = ByteBuffer.wrap((fileToUpload.getName() + "%").getBytes());
+        channel.write(fileNameBuffer);
+
+        // Read and send file in chunks
+        byte[] bufferArray = new byte[1000];
+        FileInputStream fis = new FileInputStream(fileToUpload);
+        int bytesRead;
+        while ((bytesRead = fis.read(bufferArray)) != -1) {
+            ByteBuffer fileChunkBuffer = ByteBuffer.wrap(bufferArray, 0, bytesRead);
+            channel.write(fileChunkBuffer);
+        }
+
+        channel.shutdownOutput();
+        bytesToRead = 1;
+        statusCode = ByteBuffer.allocate(bytesToRead);
+
+        while((bytesToRead -= channel.read(statusCode)) > 0);
+        statusCode.flip();
+        a = new byte[1];
+        statusCode.get(a);
+        System.out.println(new String(a));
+
+        fis.close();
+        //Implement functionality for file upload procedure
     }
 }
--- src/tcp/FileServiceServer.java
+++ src/tcp/FileServiceServer.java
@@ -2,7 +2,7 @@ package tcp;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
@@ -11,9 +11,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class FileServiceServer{
+    private static byte[] a;
+    private static String fileName;
+    private static File file;
+    private static boolean success = false;
+    private static ByteBuffer fileBytes = null;
+    private static ExecutorService executor = Executors.newFixedThreadPool(2);
+
     public static void main(String[] args) throws Exception{
 
 
@@ -42,78 +51,31 @@ public class FileServiceServer{
             request.flip();
             char command = (char) request.get();
             //Message from the client, displaying it on the server side
-            byte[] a;
-            String fileName;
-            File file;
-            boolean success = false;
-            ByteBuffer fileBytes = null;
+
             switch (command) {
                 case 'G':
                     //File to download
-                    a = new byte[request.remaining()];
-                    request.get(a);
-                    fileName = new String(a);
-                    file = new File("ServerFiles/"+fileName);
-                    if(file.exists() && file.isFile()) {
-                        byte[] fileContent = Files.readAllBytes(file.toPath());
-                        ByteBuffer fileBuffer = ByteBuffer.wrap(fileContent);
-                        ByteBuffer code = ByteBuffer.wrap("Process succeeded".getBytes());
-                        serveChannel.write(code);
-                        serveChannel.write(ByteBuffer.wrap(fileName.getBytes()));
-                        File created = new File(fileName);
-                        Files.write(created.toPath(), fileContent);
-
-                    }else {
-                        ByteBuffer code = ByteBuffer.wrap("Process Failed".getBytes());
-                        serveChannel.write(code);
-                    }
+                    executor.submit(new FileDownload(serveChannel, fileName, request));
                     break;
                 case 'U':
-                    a = new byte[request.remaining()];
-
-                    request.get(a);
-                    String totalData = new String(a);
-
-                    int separatorIndex = totalData.indexOf('%');
-
-                    if(separatorIndex == -1) {
-                        System.out.println("Invalid file format");
-                        ByteBuffer code = ByteBuffer.wrap("Process Failed".getBytes());
-                        serveChannel.write(code);
-                        break;
-                    }
-
-                    fileName = totalData.substring(0, separatorIndex);
-                    Path outputPath = Paths.get("ServerFiles", fileName);
-                    FileOutputStream fos = new FileOutputStream(outputPath.toFile(), true);  // append mode
-
-                    byte[] remainingBytes = totalData.substring(separatorIndex + 1).getBytes();
-                    fos.write(remainingBytes);
-
-                    ByteBuffer fileChunkBuffer = ByteBuffer.allocate(1000);
-                    int bytesRead;
-
-                    while ((bytesRead = serveChannel.read(fileChunkBuffer)) > 0) {
-                        fos.write(fileChunkBuffer.array(), 0, bytesRead);
-                        fileChunkBuffer.clear();
-                    }
-                    fos.close();
-
-                    ByteBuffer sCode = ByteBuffer.wrap("S".getBytes());
-                    serveChannel.write(sCode);
-                    serveChannel.write(ByteBuffer.wrap(fileName.getBytes()));
+                    executor.submit(() -> {
+                        try {
+                            handleUpload(serveChannel, request);
+                        }catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
                     break;
-
                 case 'D':
                     //Excludes the bytes that we got from the command
-                     a = new byte[request.remaining()];
+                    a = new byte[request.remaining()];
                     //Read file information
                     request.get(a);
                     fileName = new String(a);
                     file = new File("ServerFiles/"+fileName);
 
                     if(file.exists()) {
-                         success = true;
+                        success = true;
                         file.delete();
                     }
                     if(success) {
@@ -177,11 +139,97 @@ public class FileServiceServer{
             request.rewind();
             //Read from the buffer and write into the TCP channel
 //            serveChannel.write(request);
-            serveChannel.close();
+
+//            serveChannel.close();
 
             //
 
         }
 
+    }
+
+
+    public static void handleUpload(SocketChannel serveChannel, ByteBuffer request) throws IOException {
+        a = new byte[request.remaining()];
+
+        request.get(a);
+        String totalData = new String(a);
+
+        int separatorIndex = totalData.indexOf('%');
+
+        if(separatorIndex == -1) {
+            System.out.println("Invalid file format");
+            ByteBuffer code = ByteBuffer.wrap("Process Failed".getBytes());
+            serveChannel.write(code);
+            return;
+        }
+
+        fileName = totalData.substring(0, separatorIndex);
+        Path outputPath = Paths.get("ServerFiles", fileName);
+        FileOutputStream fos = new FileOutputStream(outputPath.toFile(), true);  // append mode
+
+        byte[] remainingBytes = totalData.substring(separatorIndex + 1).getBytes();
+        fos.write(remainingBytes);
+
+        ByteBuffer fileChunkBuffer = ByteBuffer.allocate(1000);
+        int bytesRead;
+
+        while ((bytesRead = serveChannel.read(fileChunkBuffer)) > 0) {
+            fos.write(fileChunkBuffer.array(), 0, bytesRead);
+            fileChunkBuffer.clear();
+        }
+
+        fos.close();
+
+
+        ByteBuffer sCode = ByteBuffer.wrap("S".getBytes());
+        serveChannel.write(sCode);
+        serveChannel.write(ByteBuffer.wrap(fileName.getBytes()));
+
+        serveChannel.close();
+    }
+
+    public static class FileDownload implements Runnable {
+        private final SocketChannel serveChannel;
+        private final String fileName;
+
+        private final ByteBuffer request;
+
+        public FileDownload(SocketChannel serveChannel, String fileName, ByteBuffer request) {
+            this.serveChannel = serveChannel;
+            this.fileName = fileName;
+            this.request = request;
+        }
+
+        @Override
+        public void run() {
+            try {
+                handleDownload(serveChannel, fileName, request);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void handleDownload(SocketChannel serveChannel, String fileName, ByteBuffer request) throws IOException {
+            a = new byte[request.remaining()];
+            request.get(a);
+            fileName = new String(a);
+            file = new File("ServerFiles/"+fileName);
+
+            System.out.println("OUR FILE: " + file);
+            if(file.exists() && file.isFile()) {
+                byte[] fileContent = Files.readAllBytes(file.toPath());
+//                ByteBuffer fileBuffer = ByteBuffer.wrap(fileContent);
+                ByteBuffer code = ByteBuffer.wrap("S".getBytes());
+                serveChannel.write(code);
+                serveChannel.write(ByteBuffer.wrap(fileName.getBytes()));
+                File created = new File(fileName);
+                Files.write(created.toPath(), fileContent);
+
+            }else {
+                ByteBuffer code = ByteBuffer.wrap("Process Failed".getBytes());
+                serveChannel.write(code);
+            }
+        }
     }
 }
